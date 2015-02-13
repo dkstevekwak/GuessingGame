728504be483245718ae0b1a66c79d42d7d569354
Steve Kwak <steveguac@gmail.com>
2015-02-13 10:43:39 -0500

    final version before submission

--- javascript.js
+++ javascript.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
     $('.submit').on('click', function(){
     	var input = +$('#input1').val();
     	count += 1;
-    	
+    	$('input').val('');
     	$('h4').text('Life: '+(5-count))
 
 		if(track.indexOf(input)!=-1) {
@@ -30,66 +30,75 @@ $(document).ready(function(){
     		$('h3').text('Correct! Congratulations!');
     		$('h4').text('Win!')
     		$('h1,h2').css('color','#FAFA43')
-    		$('.fire').animate({'height':'350px','width':'320px'},'fast');
+    		$('.fire').animate({'height':'370px','width':'320px'},'fast');
     		$('.snow').remove();
-    		$('.fireworks').fadeIn('slow')
+    		$('.fireworks').fadeIn('slow');
+    		$('.submit').attr('disabled','disabled');
+    		$('#input1').attr('disabled','disabled');
     	}
         else if(count===5) {
         	$('h3').text('You lost..play again?');
-			$('.snow').animate({'height':'350px','width':'320px'},'fast');
+			$('.snow').animate({'height':'370px','width':'320px'},'fast');
     		$('.fire').remove();
     		$('.iceage').fadeIn('slow');
     		$('h1,h2').css('color','#D3E4F5')
     	}
-    	else if(count>5) {
-    		$('h3').text('You have no more life! Play again?');
+    	else if(count===6) {
+    		$('h3').text('No more life! Play with me one more time?');
     		$('h4').html('<a href="http://faceboo.com"><p>Ask friends for more life</p></a>')
+    		$('.iceage').remove();
+    		$('.cat').fadeIn('slow');
+    		$('.snow').remove();
+    		$('.donkey').fadeIn('slow');
+    		$('.submit').attr('disabled','disabled');
+    		$('#input1').attr('disabled','disabled');
     	}
     	else if(computerinput>input) {	
 	    	if(computerinput-input>25) {
 	    		$('h3').text('You are ice cold. Guess much higher!');
-	    		$('.fire').animate({'height':'90px','width':'90px'},'fast');
-	    		$('.snow').animate({'height':'280px','width':'280px'},'fast');
+	    		$('.fire').animate({'height':'180px','width':'180px'},'fast');
+	    		$('.snow').animate({'height':'330px','width':'330px'},'fast');
 	    	}
 	    	else if(computerinput-input>15 && computerinput-input<=25) {
 	    		$('h3').text('You are cold. Guess higher!');
-	    		$('.fire').animate({'height':'120px','width':'120px'},'fast');
-	    		$('.snow').animate({'height':'210px','width':'210px'},'fast');
+	    		$('.fire').animate({'height':'220px','width':'220px'},'fast');
+	    		$('.snow').animate({'height':'300px','width':'300px'},'fast');
 	    	}
 	    	else if(computerinput-input>5 && computerinput-input<=15) {
 	    		$('h3').text('You are hot. Guess higher!');
-	    		$('.fire').animate({'height':'210px','width':'210px'},'fast');
-	    		$('.snow').animate({'height':'120px','width':'120px'},'fast');
+	    		$('.fire').animate({'height':'300px','width':'300px'},'fast');
+	    		$('.snow').animate({'height':'220px','width':'220px'},'fast');
 	    	}
 	    	else if(computerinput-input<=5) {
 	    		$('h3').text('You are super hot. Guess higher!');
-	    		$('.fire').animate({'height':'280px','width':'280px'},'fast');
-	    		$('.snow').animate({'height':'90px','width':'90px'},'fast');
+	    		$('.fire').animate({'height':'330px','width':'330px'},'fast');
+	    		$('.snow').animate({'height':'180px','width':'180px'},'fast');
  			}
  		}
     	else if(computerinput<input) {
 	    	if(input-computerinput>25) {
 	    		$('h3').text('You are ice cold. Guess much lower!');
-	    		$('.fire').animate({'height':'90px','width':'90px'},'fast');
-	    		$('.snow').animate({'height':'280px','width':'280px'},'fast');
+	    		$('.fire').animate({'height':'180px','width':'180px'},'fast');
+	    		$('.snow').animate({'height':'330px','width':'330px'},'fast');
 	    	}
 	    	else if(input-computerinput>15 && input-computerinput<=25) {
 	    		$('h3').text('You are cold. Guess lower!');
-	    		$('.fire').animate({'height':'120px','width':'120px'},'fast');
-	    		$('.snow').animate({'height':'210px','width':'210px'},'fast');
+	    		$('.fire').animate({'height':'220px','width':'220px'},'fast');
+	    		$('.snow').animate({'height':'300px','width':'300px'},'fast');
 	    	}
 	    	else if(input-computerinput>5 && input-computerinput<=15) {
 	    		$('h3').text('You are hot. Guess lower!');
-	    		$('.fire').animate({'height':'210px','width':'210px'},'fast');
-	    		$('.snow').animate({'height':'120px','width':'120px'},'fast');
+	    		$('.fire').animate({'height':'300px','width':'300px'},'fast');
+	    		$('.snow').animate({'height':'220px','width':'220px'},'fast');
 	    	}
 	    	else if(input-computerinput<=5) {
     			$('h3').text('You are super hot. Guess lower!');
-    			$('.fire').animate({'height':'280px','width':'280px'},'fast');
-	    		$('.snow').animate({'height':'90px','width':'90px'},'fast');
+    			$('.fire').animate({'height':'330px','width':'330px'},'fast');
+	    		$('.snow').animate({'height':'180px','width':'180px'},'fast');
     	    }
     	}
 		track.push(input);
+		$('h5').text("Your last input: "+input);
 	});
 
 	$('.playagain').on('click', function(){
